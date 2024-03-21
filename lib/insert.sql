334053fa87abe49bc6d234eaa17cad7fc70c7f1f
David Kerr <kerrky@gmail.com>
2024-03-20 22:21:42 -0400

    Complete Lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 10, 'M', 'brown', 'lovey', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 5, 'M', 'black', 'excited', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 7, 'F', 'black', 'chill', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 15, 'M', 'white', 'obidient', False);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 12, 'F', 'brown', 'excited', False );
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 20, 'M', 'white', 'selfish', False);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 5, 'F', 'white', 'calm', True);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 3, 'M', 'brown', 'aggressive', True);
